d098ba726e0eb689940775734a6beca1c377eda9
Liva <lclr@academy.cin.ufpe.br>
2023-02-14 16:49:31 -0300

    Initial Commit

---
+++ LearningBodyTracking/LearningBodyTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningBodyTrackingApp.swift
+//  LearningBodyTracking
+//
+//  Created by livia on 14/02/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningBodyTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
